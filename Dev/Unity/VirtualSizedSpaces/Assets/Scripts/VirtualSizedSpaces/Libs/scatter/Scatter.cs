f5adafc57d08f2858207e07eb65c8065343d52ef
Tijmen Mulder <tijmen@robotkittens.nl>
2020-06-23 21:26:16 +0200

    Created debug reference cube. Working on scattering logic.

---
+++ Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Libs/scatter/Scatter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scatter
+{
+    public Scatter(string prefab, int nr, float ypos, float xrange, float zrange) {
+        Debug.Log("Scatter!");
+    }
+}
--- Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Scene/ScenePRController.cs
+++ Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Scene/ScenePRController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ScenePRController : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        new Scatter("Cube",100,0.5f,100,100);
     }
 
     // Update is called once per frame
