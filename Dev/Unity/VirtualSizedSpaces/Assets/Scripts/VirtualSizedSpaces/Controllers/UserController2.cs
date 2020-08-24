274682db1cf0a743cd30fae8b696f4cfd14240b8
Tijmen Mulder <tijmen@robotkittens.nl>
2020-08-24 21:04:07 +0200

    Setting up second method

---
+++ Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Controllers/UserController2.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+  
+ public class UserController2 : MonoBehaviour {
+    public float speed = 1;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    private Vector3 _lastPosition;
+    float rotationX = 0F;
+    float rotationY = 0F;
+    Quaternion originalRotation;
+    float height = 2.14f;
+    private TestingResult _currentTest;
+    private Quaternion _offset =  Quaternion.Euler(0,0,0);
+
+    private void Start()
+    {
+        originalRotation = transform.localRotation;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+         angle += 360F;
+        if (angle > 360F)
+         angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    public void setSettings(TestingResult testingResult) {
+        Debug.Log("adding testresults" + testingResult);
+        _currentTest = testingResult;
+    }
+
+    void Update()
+    {
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right) ;
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion * _offset;
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            transform.localRotation = originalRotation * yQuaternion * _offset;
+        }
+
+        transform.TransformDirection(transform.localRotation.eulerAngles);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+          //  transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+           // transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime, Space.Self);
+           
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime, Space.Self);
+        }
+        transform.position = new Vector3(transform.position.x, height, transform.position.z);
+
+        if (_currentTest != null && _lastPosition != null) {
+            float distance = Vector3.Distance(_lastPosition, transform.position);
+            if (distance > 0) {
+                float change = (distance / _currentTest.Distance) * _currentTest.MaxAngle;
+                ApplyManipulation(change);
+            }
+        }
+
+        _lastPosition = transform.position;
+
+    }
+
+    private void ApplyManipulation(float val)
+    {
+        _offset *= Quaternion.Euler(0, val, 0);
+    }
+}
---
+++ Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Scene/ScenePRController2.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenePRController2 : MonoBehaviour
+{
+    private TestSettings _testSettings = new TestSettings();
+    private TestingResult _currentTest;
+    private int _currentTestKey = 10;
+    private GameObject _wallLeft;
+    private GameObject _wallRight;
+    private GameObject _wallFront;
+    private GameObject _wallBack;
+    private Scatter _scatter = new Scatter();
+    private UserController _userController;
+    private Button _selectButton;
+
+    private void Init() {
+        _wallLeft = transform.Find("Walls/left").gameObject;
+        _wallRight = transform.Find("Walls/right").gameObject;
+        _wallFront = transform.Find("Walls/front").gameObject;
+        _wallBack = transform.Find("Walls/back").gameObject;
+        _userController = transform.Find("OVRCameraRig").GetComponent<UserController>();
+        _selectButton = GameObject.Find("SelectButton").GetComponent<Button>();
+        _selectButton.onClick.AddListener(NextTest);
+    }
+
+    private void NextTest()
+    {
+        _currentTestKey++;
+        UpdateButton();
+        StartTest(_currentTestKey);
+
+    }
+
+    void Start()
+    {
+        Init();
+        StartTest(_currentTestKey);
+       
+    }
+
+    private void UpdateButton() {
+        if (_testSettings.tests.Count - 1 > _currentTestKey)
+        {
+            _selectButton.transform.Find("Text").GetComponent<Text>().text = _testSettings.tests[_currentTestKey + 1];
+            _selectButton.gameObject.SetActive(true);
+        }
+        else {
+            _selectButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void StartTest(int testnr) {
+        string _currentTestName = _testSettings.tests[testnr];
+        UpdateButton();
+
+        _currentTest = _testSettings.GetSettings(_currentTestName);
+        ApplySettings();
+    }
+
+    private void ApplySettings() {
+       
+        _wallLeft.transform.position = new Vector3(_currentTest.XWallDistance,500,0);
+        _wallRight.transform.position = new Vector3(-_currentTest.XWallDistance, 500, 0);
+        _wallFront.transform.position = new Vector3(0, 500, _currentTest.YWallDistance);
+        _wallBack.transform.position = new Vector3(0, 500, -_currentTest.YWallDistance);
+        _scatter.Make("Cube", this.gameObject, _currentTest.NrBoxes, 0.5f, 1000, 1000);
+        _userController.setSettings(_currentTest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
