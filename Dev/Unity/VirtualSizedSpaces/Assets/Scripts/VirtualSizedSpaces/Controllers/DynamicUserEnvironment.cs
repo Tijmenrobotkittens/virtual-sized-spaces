8d0a0e40ffbcde1bc704ed4aee9d22a79857065f
Tijmen Mulder <tijmen@robotkittens.nl>
2020-12-07 17:08:00 +0100

    Users detecteren en plaatsen, match raycast

--- Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Controllers/DynamicUserEnvironment.cs
+++ Dev/Unity/VirtualSizedSpaces/Assets/Scripts/VirtualSizedSpaces/Controllers/DynamicUserEnvironment.cs
@@ -17,7 +17,8 @@ public class DynamicUserEnvironment : Antilatency.InterfaceContract.InterfacedOb
     };
 
     private object _visobject = new object();
-   
+    private MatchVisualization _matchViz = new MatchVisualization();
+    private MatchByPositionVisualization _machPos = new MatchByPositionVisualization();
 
 
     private List<Vector2> _markers;
@@ -50,5 +51,88 @@ public class DynamicUserEnvironment : Antilatency.InterfaceContract.InterfacedOb
         return Vector3.Dot(up, ray) < 0;
     }
 
-    
+    Bool IEnvironment.match(Vector3[] Rays, out MarkerIndex[] Markers, out Pose PositionofUp)
+    {
+        const float projectionsMatchTolerance = 0.05f;
+
+        Markers = Enumerable.Repeat(MarkerIndex.Unknown, Rays.Length).ToArray();
+        PositionofUp = new Pose();
+
+       
+        if (Rays.Length != 3)
+            return false;
+
+        var projecties = projectRaysOnFloor(Rays);
+
+       
+        var M = new float[6, 4];
+        var b = new float[6];
+        for (int i = 0; i < 3; ++i)
+        {
+            M[2 * i, 0] = projecties[i].x;
+            M[2 * i, 1] = -projecties[i].y;
+            M[2 * i, 2] = 1;
+            M[2 * i + 1, 0] = projecties[i].y;
+            M[2 * i + 1, 1] = projecties[i].x;
+            M[2 * i + 1, 3] = 1;
+            b[2 * i] = _markers[i].x;
+            b[2 * i + 1] = _markers[i].y;
+        }
+
+       
+        var invM = M.transpose().multiply(M).inverse().multiply(M.transpose());
+
+
+        int BestMatch = -1;
+        float bestError = float.MaxValue;
+        var bestTransform2d = new ConvertTo2D();
+        for (int idPermutation = 0; idPermutation < 6; ++idPermutation)
+        {
+            var curB = new float[6];
+            for (int i = 0; i < 3; ++i)
+            {
+                int j = permutations[idPermutation][i];
+                curB[2 * i] = b[2 * j];
+                curB[2 * i + 1] = b[2 * j + 1];
+            }
+
+            var transformParams = invM.multiply(curB); 
+            var transform2d = new ConvertTo2D(transformParams[0], transformParams[1], transformParams[2], transformParams[3]);
+            float error = 0; 
+            for (int idMarker = 0; idMarker < 3; ++idMarker)
+            {
+                error += (transform2d.apply(projecties[idMarker]) - _markers[permutations[idPermutation][idMarker]]).sqrMagnitude;
+            }
+
+            if (error < bestError)
+            {
+                bestError = error;
+                BestMatch = idPermutation;
+                bestTransform2d = transform2d;
+            }
+        }
+
+        if (bestError > projectionsMatchTolerance)
+            return false;
+
+        for (int i = 0; i < Rays.Length; ++i)
+        {
+            Markers[i].value = (uint)permutations[BestMatch][i];
+        }
+
+        var position = new Vector3(bestTransform2d.Translate.x, bestTransform2d.Scale, bestTransform2d.Translate.y);
+        var rotation = Quaternion.AxisAngle(Vector3.up, -bestTransform2d.Angle);
+        PositionofUp = new Pose(position, rotation);
+
+        lock (_visobject)
+        {
+            _matchViz = new MatchVisualization(
+                Rays.ToList(),
+                PositionofUp,
+                projecties.Select(p => bestTransform2d.apply(p)).ToList(),
+                Markers);
+        }
+
+        return true;
+    }
 }
